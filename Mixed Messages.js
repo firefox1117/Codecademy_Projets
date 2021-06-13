9fa7a12968dcfa060d9dac9ef23628069e892001
firefox1117 <85685071+firefox1117@users.noreply.github.com>
2021-06-13 13:44:31 -0500

    Create Mixed Messages.js

---
+++ Mixed Messages.js
@@ -0,0 +1,37 @@
+JavaScript
+const randomNumber = num => {
+    return math.floor(Math.random() * num)
+}
+
+const greetings = {
+    choice: ["One", 'Two', 'Three'],
+    body: ['move three', 'move one', 'move six'],
+    result: ["win", 'lose', 'tie']
+}
+
+let store = [];
+
+for (let select in greetings) {
+    let randIndex = randomNumber(greetings[select].length) 
+
+    switch(select) {
+        case 'choice':
+            store.push(`your choice is ${greetings[select][randIndex]}`)
+            break
+        case 'body':
+            store.push(`You must ${greetings[select][randIndex]}`)
+            break
+        case 'result':
+            store.push(`You ${greetings[select][randIndex]}`)
+            break
+    }
+}
+
+function formatWisdom(wisdom) {
+    // Add some ASCII here?
+    // Add in more symbols to the array? like emojies or what not?
+    const formatted = personalWisdom.join('\n')
+    console.log(formatted)
+  }
+  
+  formatWisdom(personalWisdom);
